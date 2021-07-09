7c8a2d12a0b3bba132031f0abca5c0b78bb41b7d
Albeiro Marin <xlandru@hotmail.com>
2021-07-09 17:16:00 -0500

    Create getter in the class

--- index.ts
+++ index.ts
@@ -1,14 +1,38 @@
 //propiedades métodos y contructor de una clase
 class smartphone {
-  color: string;
-  brand: string;
+  private color: string;
+  private brand: string;
+  private battery: number;
 
-  constructor(color: string, brand: string) {
+  constructor(color: string, brand: string, battery: number) {
     this.color = color;
     this.brand = brand;
+    this.battery = battery;
   }
 
-  makePhoneCall(): void {}
+  makePhoneCall(): void {
+    if (this.battery === 0) throw "The phone has not battery";
+    this.battery -= 10;
+  }
+
+  get batteryLevel(): number {
+      return this.battery;
+  }
+
 }
 
-const mi10 = new smartphone("black", "xiaomi");
+const mi10 = new smartphone("black", "xiaomi", 100);
+
+mi10.makePhoneCall();
+mi10.makePhoneCall();
+mi10.makePhoneCall();
+mi10.makePhoneCall();
+mi10.makePhoneCall();
+mi10.makePhoneCall();
+mi10.makePhoneCall();
+mi10.makePhoneCall();
+mi10.makePhoneCall();
+mi10.makePhoneCall();
+mi10.makePhoneCall();
+
+
