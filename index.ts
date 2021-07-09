71f07a018b58f7b2a1db7320bab7320ff01e13ed
Albeiro Marin <xlandru@hotmail.com>
2021-07-09 16:38:09 -0500

    The class smartphone have been created

---
+++ index.ts
@@ -0,0 +1,12 @@
+//propiedades métodos y contructor de una clase
+class smartphone {
+  color: string;
+  brand: string;
+
+  constructor(color: string, brand: string) {
+    this.color = color;
+    this.brand = brand;
+  }
+
+  makePhoneCall(): void {}
+}
